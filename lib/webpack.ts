bc472e85ed665c3a33f5e8a9192361901c5867a9
tianyongsheng <tianyongsheng@bytedance.com>
2022-05-07 16:30:34 +0800

    feat: 创建比é编译器函数

--- lib/webpack.ts
+++ lib/webpack.ts
@@ -4,7 +4,7 @@ import {NodeEnvironmentPlugin} from './node/NodeEnvionmentPlugin'
 
 const webpack = (options: WebpackOptions) => {
   const create = () => {
-    // 奇怪的校验
+    // 奇怪的校验. 校验配置
     // if (!asArray(options).every(webpackOptionsSchemaCheck)) {
     //   getValidateSchema()(webpackOptionsSchema, options);
     //   util.deprecate(
@@ -19,18 +19,39 @@ const webpack = (options: WebpackOptions) => {
 
     // 1. 创建编译器
     compiler = createCompiler(webpackOptions);
-    // 2. 环境插件
-    new NodeEnvironmentPlugin({
-      infrastructureLogging: options.infrastructureLogging
-    }).apply(compiler)
+    // 2.
   }
 }
 
 const createCompiler = rawOptions => {
-  // 处理参数
-  // const options = getNormalizedWebpackOptions(rawOptions);
-  // applyWebpackOptionsBaseDefaults(options);
+  // 1. 调用 getNormalizedWebpackOptions + applyWebpackOptionsBaseDefaults 合并出最终配置
+  const options = getNormalizedWebpackOptions(rawOptions);
+  applyWebpackOptionsBaseDefaults(options);
 
   const compiler = new Compiler(options.context, options);
-
+  // 2. 环境插件
+  new NodeEnvironmentPlugin({
+    infrastructureLogging: options.infrastructureLogging
+  }).apply(compiler)
+  // 3. 注册自定义插件
+  if (Array.isArray(options.plugins)) {
+    // 遍历用户定义的 plugins 集合，执行插件的 apply 方法
+    for (const plugin of options.plugins) {
+      // 插件是函数的话， 直接调用
+      if (typeof plugin === "function") {
+        plugin.call(compiler, compiler);
+      } else {
+        // 否则，调用他的apply方法
+        plugin.apply(compiler);
+      }
+    }
+  }
+  // 4. 处理参数？ 不知道干嘛? 前边两个不够吗
+  // applyWebpackOptionsDefaults(options);
+  // 5. 在加载内部插件之前 执行两个生命周期的回调函数 environment afterEnvironment
+  compiler.hooks.environment.call();
+  compiler.hooks.afterEnvironment.call();
+  // 6. new WebpackOptionsApply().process 方法，加载各种内置插件
+  new WebpackOptionsApply().process(options, compiler);
+  compiler.hooks.initialize.call();
 }
